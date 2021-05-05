0cf3d4872a01737f51794d102c1ee014f56b6e83
techienityam <techienityam@gmail.com>
2021-05-05 16:59:04 +0530

    recapchar test

---
+++ verify.php
@@ -0,0 +1,16 @@
+<?php
+  require_once('recaptchalib.php');
+  $privatekey = "6Le0w8YaAAAAAIbOXnywo_COXMl11TL7S7oa3rBi";
+  $resp = recaptcha_check_answer ($privatekey,
+                                $_SERVER["REMOTE_ADDR"],
+                                $_POST["recaptcha_challenge_field"],
+                                $_POST["recaptcha_response_field"]);
+
+  if (!$resp->is_valid) {
+    // What happens when the CAPTCHA was entered incorrectly
+    die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+         "(reCAPTCHA said: " . $resp->error . ")");
+  } else {
+    // Your code here to handle a successful verification
+  }
+  ?>
